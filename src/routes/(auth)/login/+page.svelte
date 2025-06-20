86e90c0c1cb07aa6f1a929a3ce3e2b36f2693444
anav5704 <anavkki@gmail.com>
2025-06-21 10:18:45 +1200

    feat: add login

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { signIn } from '@/lib/auth/client';
+	import { goto } from '$app/navigation';
+
+	let email = $state('');
+	let password = $state('');
+	let errorMessage = $state('');
+
+	const handleSignUp = async () => {
+		await signIn.email({
+			email,
+			password,
+			fetchOptions: {
+				onError(context) {
+					errorMessage = context.error.message;
+				}
+			}
+		});
+
+		goto('/');
+	};
+</script>
+
+<h1 class="auth-header">Account Login</h1>
+
+<form class="card auth-form" autocomplete="off">
+	<label>
+		Email
+		<input name="email" type="email" bind:value={email} />
+	</label>
+
+	<label>
+		Password
+		<input name="password" type="password" bind:value={password} />
+	</label>
+
+	<input type="button" value="Log in" onclick={handleSignUp} />
+
+	{#if errorMessage}
+		<p class="text-rose-500">{errorMessage}</p>
+	{/if}
+</form>
+
+<p class="auth-footer">Don't have an account? <a href="/signup">Sign up</a></p>
