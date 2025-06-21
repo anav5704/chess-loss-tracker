7f3d28d1f6a0d44aa6f08609fc55fd2dc59b89d0
anav5704 <anavkki@gmail.com>
2025-06-21 19:38:21 +1200

    feat: add logout

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { signOut } from '@/lib/auth/client';
+	import { goto } from '$app/navigation';
+
+	const handleLogout = () => {
+		signOut({
+			fetchOptions: {
+				onSuccess: () => {
+					goto('/login');
+				}
+			}
+		});
+	};
+</script>
+
+<nav>
+	<button onclick={handleLogout}>Log out</button>
+</nav>
