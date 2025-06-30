8e4cd578ffe17cb03b41e6d625678bf98e5f88ce
anav5704 <anavkki@gmail.com>
2025-07-01 11:06:09 +1200

    feat: add update game

--- src/providers/ModalProvider.svelte
+++ src/providers/ModalProvider.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import CreateGame from '@/components/CreateGame.svelte';
+	import UpdateGame from '@/components/UpdateGame.svelte';
 	import { modalStore } from '@/stores/modal.svelte';
 	import Modal from '@/components/Modal.svelte';
 </script>
 
 <Modal header="Add Loss" open={modalStore.createModal}>
 	<CreateGame />
+</Modal>
+
+<Modal header="Edit Loss" open={modalStore.updateModal}>
+	<UpdateGame />
 </Modal>
